4d3adabf0eba75e0228304ce97dbbfc24e8b68cf
Patkav <patricia.kavanaugha@gmail.com>
2023-05-07 21:46:49 -0400

    added a search engine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function search(city) {
+  let apikey = "15b6ba0523386a8a73b38b2440a74dea";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get("${apiUrl}&appid=${apiKey}").then(displayTemperature);
+}
 
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
